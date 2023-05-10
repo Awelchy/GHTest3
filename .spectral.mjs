99c4efeac54c66dcb463ebe277cd09279c990c2e
amy.welch@stoplight.io <amy.welch@stoplight.io>
2023-05-10 14:05:41 -0500

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMDI0Mw/spectral.js?branch_slug=master&token=c0587799-c96e-4a1f-99e8-e3c2081570ff";
+export default { extends: ruleset };
---
+++ docs/This-is-an-Article.md
@@ -0,0 +1,9 @@
+---
+stoplight-id: 0qisxxuv9vmdt
+---
+
+# This-is-an-Article
+
+The beginning of an awesome article...
+this is to test proposales and webhooks
+thank you for working
